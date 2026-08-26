c3260a3ec37e6dee73e5ad7033380da27338b274
Caique Rodrigues <73994301+caiqrodrigues@users.noreply.github.com>
2026-08-26 11:16:22 -0300

    release(hotfix14): rotate Web cache namespace

--- apps/web/service-worker.js
+++ apps/web/service-worker.js
@@ -1,4 +1,4 @@
-const VERSION='ct-web-0.0.97-hotfix13-bingers-semantics';
+const VERSION='ct-web-0.0.97-hotfix14-physical-recovery';
 const MEDIA_CACHE=`${VERSION}-media`;
 const META_CACHE=`${VERSION}-meta`;
 
