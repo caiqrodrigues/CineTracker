cf54a5dd19f674ba96b3976e5d424df31bd565dd
Caique Rodrigues <73994301+caiqrodrigues@users.noreply.github.com>
2026-08-26 18:26:56 -0300

    chore(web): rotate HOTFIX18 cache namespace

--- apps/web/service-worker.js
+++ apps/web/service-worker.js
@@ -1,4 +1,4 @@
-const VERSION='ct-web-0.0.97-hotfix17-series-status';
+const VERSION='ct-web-0.0.97-hotfix18-documentation-governance';
 const MEDIA_CACHE=`${VERSION}-media`;
 const META_CACHE=`${VERSION}-meta`;
 
