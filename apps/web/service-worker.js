3749126574c26276851d6074d92503b506cd41e8
Caique Rodrigues <73994301+caiqrodrigues@users.noreply.github.com>
2026-08-26 08:35:24 -0300

    chore(hotfix12): rotate web cache namespace

--- apps/web/service-worker.js
+++ apps/web/service-worker.js
@@ -1,4 +1,4 @@
-const VERSION='ct-web-0.0.97-hotfix11-import-sync';
+const VERSION='ct-web-0.0.97-hotfix12-nav-mobile-import';
 const MEDIA_CACHE=`${VERSION}-media`;
 const META_CACHE=`${VERSION}-meta`;
 
